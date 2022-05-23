2e42d7de9902b1381eda0a02b57cee4e5f6e47a1
MohammedAladin <69739865+MohammedAladin@users.noreply.github.com>
2022-05-23 11:42:43 +0200

    Update DFT.py

--- DFT.py
+++ DFT.py
@@ -1,12 +1,17 @@
-#calculate DFT of a given signal
+#calculate DFT of a given signal x[n]
 import numpy as np
 
 N = int(input("Enter the number of samples: "))
-x = [None]*N
+p = int(input("Enter the number of zero padding samples : "))
+#add zero padding
+x = [0]*(N+p)
+
 for k in range(0,N):
     sum = 0
-    for n in range(0,N):
+    for n in range(0,N+p):
         x[n] = (np.sin(2*np.pi*n*k/N))
-        sum = sum + (1/N)*(np.exp(-2*np.pi*1j*k*n/N))*x[n]
+        sum = sum + x[n]*(np.exp((-2*np.pi*1j*k*n)/N))
     print(sum)
+
+#calculate inverse DFT with N samples 
 
