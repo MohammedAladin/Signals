ef3d50f22644650fbce91b5d186a0caec58d6111
MohammedAladin <69739865+MohammedAladin@users.noreply.github.com>
2022-05-23 11:14:14 +0200

    Update DFT.py

--- DFT.py
+++ DFT.py
@@ -1,11 +1,12 @@
 #calculate DFT of a given signal
 import numpy as np
-x = [None]
+
 N = int(input("Enter the number of samples: "))
+x = [None]*N
 for k in range(0,N):
     sum = 0
-
     for n in range(0,N):
-        sum = sum + (1/N)*(np.exp(-2*np.pi*1j*k*n/N))*(np.sin(2*np.pi*n*k/N))
+        x[n] = (np.sin(2*np.pi*n*k/N))
+        sum = sum + (1/N)*(np.exp(-2*np.pi*1j*k*n/N))*x[n]
     print(sum)
 
