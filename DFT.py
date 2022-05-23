1fe60e01e410f5698aaebf662e6947a4279c00dc
MohammedAladin <69739865+MohammedAladin@users.noreply.github.com>
2022-05-23 17:25:58 +0200

    Update DFT.py

--- DFT.py
+++ DFT.py
@@ -1,17 +1,26 @@
-#calculate DFT of a given signal x[n]
 import numpy as np
+#calculate DFT of a given signal x[n]
+x = [3,2] #input signal
 
-N = int(input("Enter the number of samples: "))
-p = int(input("Enter the number of zero padding samples : "))
-#add zero padding
-x = [0]*(N+p)
+def DFT(x):
+    N = len(x) #length of the signal
+    X = [0]*N #initialize X
+    for k in range(N):
+        for n in range(N):
+            X[k] += x[n]*np.exp(-2j*np.pi*n*k/N)
+    return X
+print(DFT(x))
 
-for k in range(0,N):
-    sum = 0
-    for n in range(0,N+p):
-        x[n] = (np.sin(2*np.pi*n*k/N))
-        sum = sum + x[n]*(np.exp((-2*np.pi*1j*k*n)/N))
-    print(sum)
+#recursive function to calculate FFT
+def FFT(x):
+    N = len(x)
+    if N <= 1:
+        return x
+    even = FFT(x[0::2]) #take even terms
+    odd = FFT(x[1::2]) #take odd terms
+    T = [np.exp(-2j*np.pi*k/N)*odd[k] for k in range(N//2)] 
+    return [even[k] + T[k] for k in range(N//2)] + \
+           [even[k] - T[k] for k in range(N//2)]
 
-#calculate inverse DFT with N samples 
+print(FFT(x))
 
